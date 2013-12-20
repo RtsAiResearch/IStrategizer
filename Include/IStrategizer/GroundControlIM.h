628900f01a50f1ac9f2ff6f3114775e71427abe3
MHesham <mhesham.fcis@gmail.com>
2013-12-20 15:44:31 -0800

    Fix IMs not being updated and increase Build action timeouts
    Signed-off-by: MHesham <mhesham.fcis@gmail.com>

--- Include/IStrategizer/GroundControlIM.h
+++ Include/IStrategizer/GroundControlIM.h
@@ -9,7 +9,7 @@ namespace IStrategizer
 	{
 	public:
 		GroundControlIM(IMType p_typeId) : InfluenceMap(p_typeId) {}
-		void Update(unsigned p_gameCycle);
+		void Update(const WorldClock& p_clock);
 
 	private:
 		void GetInfluence(RegObjEntry *p_pObjEntry, int &p_effectiveDistance, int &p_maxDistance, TCell &p_initValue);
