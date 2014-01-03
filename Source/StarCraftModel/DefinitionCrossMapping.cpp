0f61e10de8e196bd971ce73b84d4281669fdb7a6
MHesham <mhesham.fcis@gmail.com>
2014-01-03 03:18:21 -0800

    Fix command: game export-game-ids to export ids correctly
    Signed-off-by: MHesham <mhesham.fcis@gmail.com>

--- Source/StarCraftModel/DefinitionCrossMapping.cpp
+++ Source/StarCraftModel/DefinitionCrossMapping.cpp
@@ -12,257 +12,244 @@ DefinitionCrossMapping::DefinitionCrossMapping() : m_initialized(false) {}
 //---------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::Init()
 {
-	if (m_initialized)
-	{
-		// Because we depend on player actual Id in the game, and this class is singleton
-		// The starcraft ame may change player ids from game run to another
-		PlayerMapping.Clear();
-		InitPlayers();
-		return;
-	}
+    if (m_initialized)
+    {
+        // Because we depend on player actual Id in the game, and this class is singleton
+        // The starcraft ame may change player ids from game run to another
+        PlayerMapping.Clear();
+        InitPlayers();
+        return;
+    }
 
-	InitEntities();
-	InitUpgrades();
-	InitTeches();
-	InitPlayers();
-	InitActions();
-	InitEntityIdents();
-	InitUpgradeIdents();
-	InitTechIdents();
-  InitActions();
+    InitEntities();
+    InitUpgrades();
+    InitTeches();
+    InitPlayers();
+    InitActions();
+    InitEntityIdents();
+    InitUpgradeIdents();
+    InitTechIdents();
+    InitActions();
 
-	m_initialized = true;
+    m_initialized = true;
 }
 //---------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitEntities()
 {
-	vector< pair<TID, EntityClassType> > entities;
-	const char *name;
+    vector< pair<TID, EntityClassType> > entities;
+    const char *name;
 
-	int currentId = (int)ECLASS_START;
-	for(UnitType::set::iterator i = UnitTypes::allUnitTypes().begin();
-		i != UnitTypes::allUnitTypes().end();
-		++i)
-	{
-		entities.push_back(make_pair((*i).getID(), (EntityClassType)currentId));
+    int currentId = (int)ECLASS_START;
+    for(UnitType::set::iterator i = UnitTypes::allUnitTypes().begin();
+        i != UnitTypes::allUnitTypes().end();
+        ++i)
+    {
+        entities.push_back(make_pair((*i).getID(), (EntityClassType)currentId));
 
-		name = (*i).getName().c_str();
-		Enums[currentId] = _strdup(name);
+        name = (*i).getName().c_str();
+        Enums[currentId] = _strdup(name);
 
-		++currentId;
-	}
+        ++currentId;
+    }
 
-	EntityMapping = CrossMap<TID, EntityClassType>(entities);
+    EntityMapping = CrossMap<TID, EntityClassType>(entities);
 }
 //---------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitUpgrades()
 {
-	vector< pair<TID, ResearchType> > upgrades;
-	const char *name;
+    vector< pair<TID, ResearchType> > upgrades;
+    const char *name;
 
-	int currentId = (int)RESEARCH_START;
-	assert(UpgradeTypes::allUpgradeTypes().size() < (COUNT(ResearchType) / 2));
+    int currentId = (int)RESEARCH_START;
+    assert(UpgradeTypes::allUpgradeTypes().size() < (COUNT(ResearchType) / 2));
 
-	for(UpgradeType::set::iterator i = UpgradeTypes::allUpgradeTypes().begin();
-		i != UpgradeTypes::allUpgradeTypes().end();
-		++i)
-	{
-		upgrades.push_back(make_pair((*i).getID(), (ResearchType)currentId));
+    for(UpgradeType::set::iterator i = UpgradeTypes::allUpgradeTypes().begin();
+        i != UpgradeTypes::allUpgradeTypes().end();
+        ++i)
+    {
+        upgrades.push_back(make_pair((*i).getID(), (ResearchType)currentId));
 
-		name = (*i).getName().c_str();
-		Enums[currentId] = _strdup(name);
+        name = (*i).getName().c_str();
+        Enums[currentId] = _strdup(name);
 
-		++currentId;
-	}
+        ++currentId;
+    }
 
-	UpgradeMapping = CrossMap<TID, ResearchType>(upgrades);
+    UpgradeMapping = CrossMap<TID, ResearchType>(upgrades);
 }
 //--------------------------------------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitTeches()
 {
-	vector< pair<TID, ResearchType> > m_teches;
-	const char *name;
+    vector< pair<TID, ResearchType> > m_teches;
+    const char *name;
 
-	int currentId = (int)RESEARCH_START + TechIdOffset;
-	assert(TechTypes::allTechTypes().size() < (COUNT(ResearchType) / 2));
+    int currentId = (int)RESEARCH_START + TechIdOffset;
+    assert(TechTypes::allTechTypes().size() < (COUNT(ResearchType) / 2));
 
-	for(TechType::set::iterator i = TechTypes::allTechTypes().begin();
-		i != TechTypes::allTechTypes().end();
-		++i)
-	{
-		m_teches.push_back(make_pair((*i).getID(), (ResearchType)currentId));
+    for(TechType::set::iterator i = TechTypes::allTechTypes().begin();
+        i != TechTypes::allTechTypes().end();
+        ++i)
+    {
+        m_teches.push_back(make_pair((*i).getID(), (ResearchType)currentId));
 
-		name = (*i).getName().c_str();
-		Enums[currentId] = _strdup(name);
+        name = (*i).getName().c_str();
+        Enums[currentId] = _strdup(name);
 
-		++currentId;
-	}
+        ++currentId;
+    }
 
-	TechMapping = CrossMap<TID, ResearchType>(m_teches);
+    TechMapping = CrossMap<TID, ResearchType>(m_teches);
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitPlayers()
 {
-	vector< pair<TID, IStrategizer::PlayerType> > m_players;
-	const Playerset &players = Broodwar->getPlayers();
-	BWAPI::Player pPlayer;
+    vector< pair<TID, IStrategizer::PlayerType> > m_players;
+    const Playerset &players = Broodwar->getPlayers();
+    BWAPI::Player pPlayer;
 
-	for (Playerset::iterator i = players.begin();
-    i != players.end();
-		++i)
-	{
-		pPlayer = (*i);
+    for (Playerset::iterator i = players.begin();
+        i != players.end();
+        ++i)
+    {
+        pPlayer = (*i);
 
-    // FIXME: PLAYER_Self is not valid if there are more than 1 human player
-    // for example in replays or network game
-    // Player types should be removed from the engine and only Player IDs should be used
-		if(pPlayer->getType().getID() == PlayerTypes::Player.getID())
-			m_players.push_back(make_pair(pPlayer->getID(), PLAYER_Self));
-		else if(pPlayer->getType().getID() == PlayerTypes::Computer.getID())
-			m_players.push_back(make_pair(pPlayer->getID(), PLAYER_Enemy));
-		else if(pPlayer->isNeutral())
-			m_players.push_back(make_pair(pPlayer->getID(), PLAYER_Neutral));
-	}
+        // FIXME: PLAYER_Self is not valid if there are more than 1 human player
+        // for example in replays or network game
+        // Player types should be removed from the engine and only Player IDs should be used
+        if(pPlayer->getType().getID() == PlayerTypes::Player.getID())
+            m_players.push_back(make_pair(pPlayer->getID(), PLAYER_Self));
+        else if(pPlayer->getType().getID() == PlayerTypes::Computer.getID())
+            m_players.push_back(make_pair(pPlayer->getID(), PLAYER_Enemy));
+        else if(pPlayer->isNeutral())
+            m_players.push_back(make_pair(pPlayer->getID(), PLAYER_Neutral));
+    }
 
-	PlayerMapping = CrossMap<TID, IStrategizer::PlayerType>(m_players);
+    PlayerMapping = CrossMap<TID, IStrategizer::PlayerType>(m_players);
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitEntityIdents()
 {
-	vector< pair<TID, TName> > entities;
+    vector< pair<TID, TName> > entities;
 
-	for(UnitType::set::iterator i = UnitTypes::allUnitTypes().begin();
-		i != UnitTypes::allUnitTypes().end();
-		++i)
-	{
-		entities.push_back(make_pair((*i).getID(), (*i).getName()));
-	}
+    for(UnitType::set::iterator i = UnitTypes::allUnitTypes().begin();
+        i != UnitTypes::allUnitTypes().end();
+        ++i)
+    {
+        entities.push_back(make_pair((*i).getID(), (*i).getName()));
+    }
 
-	EntityIdentMapping = CrossMap<TID, TName>(entities);
+    EntityIdentMapping = CrossMap<TID, TName>(entities);
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitUpgradeIdents()
 {
-	vector< pair<TID, TName> > m_upgrades;
+    vector< pair<TID, TName> > m_upgrades;
 
-	for(UpgradeType::set::iterator i = UpgradeTypes::allUpgradeTypes().begin();
-		i != UpgradeTypes::allUpgradeTypes().end();
-		++i)
-	{
-		m_upgrades.push_back(make_pair((*i).getID(), (*i).getName()));
-	}
+    for(UpgradeType::set::iterator i = UpgradeTypes::allUpgradeTypes().begin();
+        i != UpgradeTypes::allUpgradeTypes().end();
+        ++i)
+    {
+        m_upgrades.push_back(make_pair((*i).getID(), (*i).getName()));
+    }
 
-	UpgradeIdentMapping = CrossMap<TID, TName>(m_upgrades);
+    UpgradeIdentMapping = CrossMap<TID, TName>(m_upgrades);
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitTechIdents()
 {
-	vector< pair<TID, TName> > m_teches;
+    vector< pair<TID, TName> > m_teches;
 
-	for(TechType::set::iterator i = TechTypes::allTechTypes().begin();
-		i != TechTypes::allTechTypes().end();
-		++i)
-	{
-		m_teches.push_back(make_pair((*i).getID(), (*i).getName()));
-	}
+    for(TechType::set::iterator i = TechTypes::allTechTypes().begin();
+        i != TechTypes::allTechTypes().end();
+        ++i)
+    {
+        m_teches.push_back(make_pair((*i).getID(), (*i).getName()));
+    }
 
-	TechIdentMapping = CrossMap<TID, TName>(m_teches);
+    TechIdentMapping = CrossMap<TID, TName>(m_teches);
 }
 //----------------------------------------------------------------------------------------------
 bool DefinitionCrossMapping::ExportGameIds(string p_exportPath)
 {
-	fstream pen;
-	stringstream stream;
-	pen.open(p_exportPath.c_str(), ios::out);
+    fstream pen;
+    stringstream stream;
+    const char* name;
+    pen.open(p_exportPath.c_str(), ios::out);
 
-	if (!pen.is_open())
-		return false;
+    if (!pen.is_open())
+        return false;
 
-	vector<TID> ids;
-	EntityIdentMapping.FirstValues(ids);
-	for(unsigned i = 0; i < ids.size(); ++i)
-	{
-		stream << EntityIdentMapping.GetByFirst(ids[i]);
-		if(i == 0)
-			stream << ' ' << EntityMapping.GetByFirst(ids[i]);
-	}
+    // TODO: Looping can be smart than this by looping only on required types
+    // without looping on the while ENUMS_SIZE
+    for (unsigned id = 0; id < ENUMS_SIZE; ++id)
+    {
+        if (BELONG(EntityClassType, id) ||
+            BELONG(ResearchType, id))
+        {
+            name = Enums[id];
 
-	stream << endl;
+            // Has a name defined
+            if (name != nullptr)
+            {
+                stream << id << ' ' << name << endl;
+            }
+        }
+    }
 
-	ids.clear();
-	UpgradeIdentMapping.FirstValues(ids);
-	for(unsigned i = 0; i < ids.size(); ++i)
-	{
-		stream << UpgradeIdentMapping.GetByFirst(ids[i]);
-		if(i == 0)
-			stream << ' ' << UpgradeMapping.GetByFirst(ids[i]);
-	}
+    pen << stream.str();
+    pen.close();
 
-	stream << endl;
-
-	ids.clear();
-	TechIdentMapping.FirstValues(ids);
-	for(unsigned i = 0; i < ids.size(); ++i)
-	{
-		stream << TechIdentMapping.GetByFirst(ids[i]);
-		if(i == 0)
-			stream << ' ' << TechMapping.GetByFirst(ids[i]);
-	}
-
-	pen << stream.str();
-	pen.close();
-
-	return true;
+    return true;
 }
 //----------------------------------------------------------------------------------------------
 bool DefinitionCrossMapping::ExportAllIds(string p_exportPath)
 {
-	fstream pen;
-	stringstream stream;
-	const char* name;
-	pen.open(p_exportPath.c_str(), ios::out);
+    fstream pen;
+    stringstream stream;
+    const char* name;
+    pen.open(p_exportPath.c_str(), ios::out);
 
-	if (!pen.is_open())
-		return false;
+    if (!pen.is_open())
+        return false;
 
-	for (unsigned id = 0; id < ENUMS_SIZE; ++id)
-	{
-		name = Enums[id];
+    for (unsigned id = 0; id < ENUMS_SIZE; ++id)
+    {
+        name = Enums[id];
 
-		// Has a name defined
-		if (name != nullptr)
-		{
-			stream << id << ' ' << name << endl;
-		}
-	}
+        // Has a name defined
+        if (name != nullptr)
+        {
+            stream << id << ' ' << name << endl;
+        }
+    }
 
-	pen << stream.str();
-	pen.close();
+    pen << stream.str();
+    pen.close();
 
-	return true;
+    return true;
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::EntityTypes(vector<EntityClassType>& p_entityTypes)
 {
-	EntityMapping.SecondValues(p_entityTypes);
+    EntityMapping.SecondValues(p_entityTypes);
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::ResearchTypes(vector<IStrategizer::ResearchType>& p_researchTypes)
 {
-	UpgradeMapping.SecondValues(p_researchTypes);
-	TechMapping.SecondValues(p_researchTypes, true);
+    UpgradeMapping.SecondValues(p_researchTypes);
+    TechMapping.SecondValues(p_researchTypes, true);
 }
 //----------------------------------------------------------------------------------------------
 void DefinitionCrossMapping::InitActions()
 {
-  vector< pair<TID, ActionType> > actions;
+    vector< pair<TID, ActionType> > actions;
 
-  actions.push_back(make_pair(Orders::AttackMove.getID(), ACTIONEX_AttackGround));
-  actions.push_back(make_pair(Orders::AttackUnit.getID(), ACTIONEX_AttackEntity));
-  actions.push_back(make_pair(Orders::ResearchTech.getID(), ACTIONEX_Research));
-  actions.push_back(make_pair(Orders::Upgrade.getID(), ACTIONEX_Research));
-  actions.push_back(make_pair(Orders::PlaceBuilding.getID(), ACTIONEX_BuildEx));
-  actions.push_back(make_pair(Orders::Train.getID(), ACTIONEX_Train));
+    actions.push_back(make_pair(Orders::AttackMove.getID(), ACTIONEX_AttackGround));
+    actions.push_back(make_pair(Orders::AttackUnit.getID(), ACTIONEX_AttackEntity));
+    actions.push_back(make_pair(Orders::ResearchTech.getID(), ACTIONEX_Research));
+    actions.push_back(make_pair(Orders::Upgrade.getID(), ACTIONEX_Research));
+    actions.push_back(make_pair(Orders::PlaceBuilding.getID(), ACTIONEX_BuildEx));
+    actions.push_back(make_pair(Orders::Train.getID(), ACTIONEX_Train));
 
-  ActionMapping = CrossMap<TID, IStrategizer::ActionType>(actions);
+    ActionMapping = CrossMap<TID, IStrategizer::ActionType>(actions);
 }
