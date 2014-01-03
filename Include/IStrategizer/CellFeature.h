b99df6f097a7637d7e8c9957632a8d8e9ea8f0b4
Abdelrahman Elogeel <zikas_zak@hotmail.com>
2014-01-03 02:18:46 -0800

    Apply CR

--- Include/IStrategizer/CellFeature.h
+++ Include/IStrategizer/CellFeature.h
@@ -24,7 +24,6 @@ namespace IStrategizer
 		double					m_distanceFromEnemyBase;
 		double					m_distanceFromBase;
 
-	public:
 				CellFeature() { Clear(); }
 				CellFeature(const PlanStepParameters& p_parameters);
 		void	AddEntity(GameEntity *p_entity,bool p_isAllied);
@@ -32,11 +31,13 @@ namespace IStrategizer
 		void	Clear();
 		void	To(PlanStepParameters& p_parameters) const;
 		void	CalculateDistanceToBases(Vector2 cellWorldPosition);
-		void	CalculateDistanceToBasesAux(Vector2 cellWorldPosition, vector<TID> bases, double& distance);
-		float	GetBaseDistance(double firstBase, double secondBase) const;
 		float	GetDistance(CellFeature *p_other);
 
 		static const CellFeature& Null() { static CellFeature nullCellFeature; return nullCellFeature; }
+
+	private:
+		void	CalculateDistanceToBasesAux(Vector2 cellWorldPosition, vector<TID> bases, double& distance);
+		float	GetBaseDistance(double firstBase, double secondBase) const;
     };
 }
 
